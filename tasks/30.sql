f23f5eabfe546d3df048155d19a10f5c87a7212a
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2023-02-09 22:04:11 +0300

    task 30

---
+++ tasks/30.sql
@@ -0,0 +1,7 @@
+SELECT
+    MAX(SALARY*MONTHS),
+    COUNT(SALARY*MONTHS)
+FROM EMPLOYEE
+WHERE (SALARY*MONTHS) = (SELECT
+                            MAX(SALARY*MONTHS)
+                         FROM EMPLOYEE);
